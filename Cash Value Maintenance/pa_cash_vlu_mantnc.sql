5c43455a5f06e21cb3d1055bd340c74d0a0fc6f6
Schiff György at Blackbelt <gyorgy.schiff@blackbelt.hu>
2016-10-21 10:23:39 +0200

    Duplicate code refactor and handling null input value as "no change" for r_factor

--- Cash Value Maintenance/pa_cash_vlu_mantnc.sql
+++ Cash Value Maintenance/pa_cash_vlu_mantnc.sql
@@ -6,30 +6,9 @@ PACKAGE CASH_VLU_MANTNC IS
   * Date         : 12/10/2016
   * Description  : First created 
   ******************************************************/
-
-  PROCEDURE SET_CASH_VAL_R_FACTOR(p_mrkt_id IN NUMBER,
-                                  p_sls_perd_id IN NUMBER,
-                                  p_cash_val IN NUMBER,
-                                  p_r_factor IN NUMBER,
-                                  p_user_id IN VARCHAR2,
-                                  p_stus OUT NUMBER);
-                                  
-  FUNCTION GET_CASH_VAL(p_mrkt_id IN NUMBER,
-                        p_sls_perd_list IN NUMBER_ARRAY
-                        ) RETURN OBJ_CASH_VAL_MANTNC_TABLE PIPELINED;                                
-  
-END CASH_VLU_MANTNC;
-
-
-create or replace 
-PACKAGE BODY CASH_VLU_MANTNC AS
-  /*********************************************************
-  * History
-  * Created by   : Schiff Gy
-  * Date         : 12/10/2016
-  * Description  : First created 
-  ******************************************************/
 FUNCTION get_local_prcsng_dt(p_mrkt_id NUMBER, p_sls_perd NUMBER) RETURN DATE;
+
+FUNCTION get_latest_prcsng_dt(p_mrkt_id NUMBER, p_sls_perd NUMBER) RETURN DATE;
 
 FUNCTION GET_CASH_VAL(p_mrkt_id IN NUMBER,
                       p_sls_perd_list IN NUMBER_ARRAY
@@ -68,6 +47,8 @@ PROCEDURE SET_CASH_VAL_R_FACTOR(p_mrkt_id IN NUMBER,
                                   p_user_id IN VARCHAR2,
                                   p_stus OUT NUMBER) IS
   /*********************************************************
+  * INPUT p_r_factor IS NULL handled as do nothing with this field
+  *
   * Possible OUT Values
   * 0 - success
   * 1 - New CASH_VAL value is negative
@@ -81,9 +62,10 @@ PROCEDURE SET_CASH_VAL_R_FACTOR(p_mrkt_id IN NUMBER,
   old_sct_r_factor MRKT_SLS_PERD.SCT_R_FACTOR%TYPE;
   mrkt_perd_exists NUMBER;
   wtd CHAR(1):='N';
+  ignore_r_factor BOOLEAN:=p_r_factor IS NULL;
 BEGIN
   p_stus:=0;
-  IF p_cash_val>0 THEN p_STUS:=1;
+  IF p_cash_val<0 THEN p_STUS:=1;
   ELSE
     SELECT MAX(SLS_PERD_ID) INTO max_perd_id
       FROM DLY_BILNG WHERE MRKT_ID=p_mrkt_id
@@ -97,7 +79,7 @@ BEGIN
         INTO  old_sct_cash_val,old_sct_r_factor
         FROM MRKT_SLS_PERD WHERE MRKT_ID=p_mrkt_id AND SLS_PERD_ID=p_sls_perd_id;
 -- If either the Cash Value or the R-Factor is different to current values
-        IF old_sct_cash_val=p_cash_val AND old_sct_r_factor=p_r_factor
+        IF old_sct_cash_val=p_cash_val AND (ignore_r_factor OR old_sct_r_factor=p_r_factor)
           THEN wtd:='N'; -- No changes, nothing to do
           ELSE wtd:='U'; -- previous value exists and different, so update
         END IF;
@@ -114,23 +96,30 @@ BEGIN
       ELSE
 -- Create the missing local processing date value
         local_prcsng_dt:=CASH_VLU_MANTNC.get_local_prcsng_dt(p_mrkt_id, p_sls_perd_id);
+        DECLARE
+          r_factor_to_set NUMBER;
         BEGIN
+          IF ignore_r_factor THEN 
+            r_factor_to_set:=nvl(old_sct_r_factor,NULL);
+          ELSE 
+            r_factor_to_set:=p_r_factor;
+          END IF;
           SAVEPOINT before_changes;
 -- Upsert the appropriate record in MRKT_SLS_PERD to set new values including local processing date
           IF wtd = 'U' THEN
             UPDATE MRKT_SLS_PERD
             SET MRKT_SLS_PERD.SCT_CASH_VAL=p_cash_val,
-                MRKT_SLS_PERD.SCT_R_FACTOR=p_r_factor,
+                MRKT_SLS_PERD.SCT_R_FACTOR=r_factor_to_set,
                 MRKT_SLS_PERD.SCT_PRCSNG_DT=local_prcsng_dt,
                 MRKT_SLS_PERD.LAST_UPDT_USER_ID=p_user_id
               WHERE MRKT_ID=p_mrkt_id AND SLS_PERD_ID=p_sls_perd_id;
           ELSE
-            INSERT INTO MRKT_SLS_PERD(MRKT_ID,SLS_PERD_ID,SCT_CASH_VAL,SCT_PRCSNG_DT,LAST_UPDT_USER_ID)
-              VALUES (p_mrkt_id,p_sls_perd_id,p_cash_val,local_prcsng_dt,p_user_id);
+            INSERT INTO MRKT_SLS_PERD(MRKT_ID,SLS_PERD_ID,SCT_CASH_VAL,SCT_R_FACTOR,SCT_PRCSNG_DT,LAST_UPDT_USER_ID)
+              VALUES (p_mrkt_id,p_sls_perd_id,p_cash_val,r_factor_to_set,local_prcsng_dt,p_user_id);
           END IF;
 -- Insert a record into CASH_VAL_RF_HIST to record the previous values
-          INSERT INTO CASH_VAL_RF_HIST(MRKT_ID,SLS_PERD_ID,CASH_VAL,PRCSNG_DT,LAST_UPDT_USER_ID)
-            SELECT MRKT_ID,SLS_PERD_ID,p_cash_val,local_prcsng_dt,p_user_id 
+          INSERT INTO CASH_VAL_RF_HIST(MRKT_ID,SLS_PERD_ID,CASH_VAL,R_FACTOR,PRCSNG_DT,LAST_UPDT_USER_ID)
+            SELECT MRKT_ID,SLS_PERD_ID,p_cash_val,r_factor_to_set,local_prcsng_dt,p_user_id 
               FROM MRKT_SLS_PERD WHERE MRKT_ID=p_mrkt_id AND SLS_PERD_ID=p_sls_perd_id;
         EXCEPTION WHEN OTHERS THEN ROLLBACK TO before_changes; p_stus:=2;
         END;
@@ -161,20 +150,26 @@ FUNCTION get_local_prcsng_dt(p_mrkt_id NUMBER, p_sls_perd NUMBER) RETURN DATE IS
       INTO actual_sls_perd, before2_sls_perd, bst_bilng_dt, est_bilng_dt
       FROM mrkt_perd2 a1 LEFT JOIN mrkt_perd3 a2 ON a2.rn=a1.rn-2
       WHERE a1.PERD_ID=p_sls_perd;
-    SELECT max(PRCSNG_DT) INTO latest_prcsng_dt
-      FROM DLY_BILNG WHERE MRKT_ID=p_mrkt_id AND SLS_PERD_ID in(actual_sls_perd)
-      GROUP BY MRKT_ID,SLS_PERD_ID; 
+    latest_prcsng_dt:=get_latest_prcsng_dt(p_mrkt_id,actual_sls_perd);
     IF latest_prcsng_dt IS NOT NULL THEN 
       processing_dt:=LEAST(latest_prcsng_dt,bst_bilng_dt);
     ELSIF before2_sls_perd IS NOT NULL THEN
-      SELECT max(PRCSNG_DT) INTO latest_prcsng_dt
-        FROM DLY_BILNG WHERE MRKT_ID=p_mrkt_id AND SLS_PERD_ID in(before2_sls_perd)
-        GROUP BY MRKT_ID,SLS_PERD_ID; 
+      latest_prcsng_dt:=get_latest_prcsng_dt(p_mrkt_id,before2_sls_perd);
       IF latest_prcsng_dt IS NOT NULL THEN 
         processing_dt:=LEAST(latest_prcsng_dt,est_bilng_dt);
       END IF;
     END IF;  
     RETURN processing_dt;
+END;
+
+FUNCTION get_latest_prcsng_dt(p_mrkt_id NUMBER, p_sls_perd NUMBER) RETURN DATE IS
+  latest_prcsng_dt date;
+BEGIN
+  SELECT max(PRCSNG_DT) INTO latest_prcsng_dt
+    FROM DLY_BILNG WHERE MRKT_ID=p_mrkt_id AND SLS_PERD_ID=p_sls_perd
+    GROUP BY MRKT_ID,SLS_PERD_ID;
+  RETURN latest_prcsng_dt; 
+EXCEPTION WHEN NO_DATA_FOUND THEN RETURN NULL;
 END;
 
 END CASH_VLU_MANTNC;
