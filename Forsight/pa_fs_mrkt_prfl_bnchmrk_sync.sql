b46f965f4dbe278a0ff3c6cf4234852dc1b5ff3c
Schiff György at Blackbelt <gyorgy.schiff@blackbelt.hu>
2016-10-24 13:13:59 +0200

    Deencapsulation of parameters.

--- Forsight/pa_fs_mrkt_prfl_bnchmrk_sync.sql
+++ Forsight/pa_fs_mrkt_prfl_bnchmrk_sync.sql
@@ -1,6 +1,16 @@
 create or replace PACKAGE FS_MRKT_PRFL_BNCHMRK_SYNC AS 
-  
-  PROCEDURE SYNCH_BNCHMRK(p_parm IN FS_MRKT_PRFL_BNCHMRK_LINE);
+  /*********************************************************
+  * History
+  * Created by   : Schiff Gy
+  * Date         : 12/10/2016
+  * Description  : First created 
+  ******************************************************/
+
+  PROCEDURE SYNC_BNCHMRK(p_MRKT_ID number,
+     p_PRFL_CD number,
+     p_EFF_PERD_ID number,
+     p_user_id IN VARCHAR2,
+     BENCHMARK_DATA FS_MRKT_PRFL_BNCHMRK_DTA_TABLE);
 
 END FS_MRKT_PRFL_BNCHMRK_SYNC;
 
@@ -13,8 +23,12 @@ create or replace PACKAGE BODY FS_MRKT_PRFL_BNCHMRK_SYNC AS
   * Depends on   : Type Number_Array
   ******************************************************/
 
-  PROCEDURE SYNC_BNCHMRK(p_parm IN FS_MRKT_PRFL_BNCHMRK_LINE) AS
-    p_data_count number :=p_parm.BENCHMARK_DATA.count;
+  PROCEDURE SYNC_BNCHMRK(p_MRKT_ID number,
+      p_PRFL_CD number,
+      p_EFF_PERD_ID number,
+      p_user_id IN VARCHAR2,
+      BENCHMARK_DATA FS_MRKT_PRFL_BNCHMRK_DTA_TABLE) AS
+    p_data_count number :=BENCHMARK_DATA.count;
     used_bnchmrk_prfl_codes NUMBER_ARRAY:=NUMBER_ARRAY();
   BEGIN
 -- If no benchmark_profile_code referred in input,
@@ -22,28 +36,29 @@ create or replace PACKAGE BODY FS_MRKT_PRFL_BNCHMRK_SYNC AS
 --   then add a new record, where benchmark_profile_code is the same as the profile_code and the default_indicator is set to 'yes' ('Y')
     IF p_data_count=0 THEN
       DELETE FROM FS_MRKT_PRFL_BNCHMRK
-        WHERE MRKT_ID=p_parm.MRKT_ID AND PRFL_CD=p_parm.PRFL_CD AND EFF_PERD_ID= p_parm.EFF_PERD_ID;
-      INSERT INTO FS_MRKT_PRFL_BNCHMRK(MRKT_ID,PRFL_CD,EFF_PERD_ID,BNCHMRK_PRFL_CD,DFALT_IND)
-        VALUES (p_parm.MRKT_ID,p_parm.PRFL_CD,p_parm.EFF_PERD_ID,p_parm.PRFL_CD,'Y');
+        WHERE MRKT_ID=p_MRKT_ID AND PRFL_CD=p_PRFL_CD AND EFF_PERD_ID=p_EFF_PERD_ID;
+      INSERT INTO FS_MRKT_PRFL_BNCHMRK(MRKT_ID,PRFL_CD,EFF_PERD_ID,BNCHMRK_PRFL_CD,DFALT_IND,CREAT_USER_ID,LAST_UPDT_USER_ID)
+        VALUES (p_MRKT_ID,p_PRFL_CD,p_EFF_PERD_ID,p_PRFL_CD,'Y',p_user_id,p_user_id);
     ELSE
 -- else insert new and modify the existing records if needed
       FOR i IN 1..p_data_count LOOP
         MERGE INTO FS_MRKT_PRFL_BNCHMRK trgt
-          USING (SELECT p_parm.MRKT_ID,p_parm.PRFL_CD,p_parm.EFF_PERD_ID,
-                        p_parm.BENCHMARK_DATA(i).BNCHMRK_PRFL_CD BNCHMRK_PRFL_CD from dual) src
-            ON (p_parm.MRKT_ID=trgt.MRKT_ID AND p_parm.PRFL_CD=trgt.PRFL_CD AND p_parm.EFF_PERD_ID=trgt.EFF_PERD_ID
+          USING (SELECT p_MRKT_ID,p_PRFL_CD,p_EFF_PERD_ID,
+                        BENCHMARK_DATA(i).BNCHMRK_PRFL_CD BNCHMRK_PRFL_CD from dual) src
+            ON (p_MRKT_ID=trgt.MRKT_ID AND p_PRFL_CD=trgt.PRFL_CD AND p_EFF_PERD_ID=trgt.EFF_PERD_ID
                                               AND src.BNCHMRK_PRFL_CD=trgt.BNCHMRK_PRFL_CD)
           WHEN MATCHED THEN
-            UPDATE SET trgt.DFALT_IND = p_parm.BENCHMARK_DATA(i).DFALT_IND WHERE trgt.DFALT_IND!=p_parm.BENCHMARK_DATA(i).DFALT_IND
+            UPDATE SET trgt.DFALT_IND = BENCHMARK_DATA(i).DFALT_IND, trgt.LAST_UPDT_USER_ID=p_user_id
+              WHERE trgt.DFALT_IND!=BENCHMARK_DATA(i).DFALT_IND
           WHEN NOT MATCHED THEN
-            INSERT (MRKT_ID,PRFL_CD,EFF_PERD_ID,BNCHMRK_PRFL_CD,DFALT_IND)
-              VALUES (p_parm.MRKT_ID,p_parm.PRFL_CD,p_parm.EFF_PERD_ID,src.BNCHMRK_PRFL_CD,p_parm.BENCHMARK_DATA(i).DFALT_IND);
+            INSERT (MRKT_ID,PRFL_CD,EFF_PERD_ID,BNCHMRK_PRFL_CD,DFALT_IND,CREAT_USER_ID,LAST_UPDT_USER_ID)
+              VALUES (p_MRKT_ID,p_PRFL_CD,p_EFF_PERD_ID,src.BNCHMRK_PRFL_CD,BENCHMARK_DATA(i).DFALT_IND,p_user_id,p_user_id);
         used_bnchmrk_prfl_codes.extend();
-        used_bnchmrk_prfl_codes(used_bnchmrk_prfl_codes.count):=p_parm.BENCHMARK_DATA(i).BNCHMRK_PRFL_CD;
+        used_bnchmrk_prfl_codes(used_bnchmrk_prfl_codes.count):=BENCHMARK_DATA(i).BNCHMRK_PRFL_CD;
       END LOOP;
 --   then delete the records identified by market_id, profile_code, effective_period_id and benchmark_profile_code not mentioned in the input (p_parm).      
       DELETE FROM FS_MRKT_PRFL_BNCHMRK
-        WHERE MRKT_ID=p_parm.MRKT_ID AND PRFL_CD=p_parm.PRFL_CD AND EFF_PERD_ID= p_parm.EFF_PERD_ID
+        WHERE MRKT_ID=p_MRKT_ID AND PRFL_CD=p_PRFL_CD AND EFF_PERD_ID=p_EFF_PERD_ID
           AND BNCHMRK_PRFL_CD NOT IN (select column_value from table( used_bnchmrk_prfl_codes ));
     END IF;
     NULL;
