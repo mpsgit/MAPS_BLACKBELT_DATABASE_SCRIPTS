fa36a84e554dfd858864015bb5f32d366cf5b842
Schiff György at Blackbelt <gyorgy.schiff@blackbelt.hu>
2016-11-03 16:22:22 +0100

    Refresh functionality and speed refactor

--- BI24 Units Adjustment/pa_dly_bilng_adjstmnt.sql
+++ BI24 Units Adjustment/pa_dly_bilng_adjstmnt.sql
@@ -17,6 +17,9 @@ create or replace PACKAGE PA_DLY_BILNG_ADJSTMNT AS
                         p_prcsng_dt IN DATE
                         ) RETURN OBJ_DLY_BILNG_ADJSTMNT_TABLE PIPELINED;                                
 
+  FUNCTION GET_DLY_BILNG_ADJSTMNT2(p_dly_bilng_id_list IN NUMBER_ARRAY)
+    RETURN OBJ_DLY_BILNG_ADJSTMNT_TABLE PIPELINED;                                
+
 END PA_DLY_BILNG_ADJSTMNT;
 
 create or replace PACKAGE BODY PA_DLY_BILNG_ADJSTMNT AS
@@ -26,8 +29,24 @@ create or replace PACKAGE BODY PA_DLY_BILNG_ADJSTMNT AS
                         p_offr_perd_id IN NUMBER,
                         p_prcsng_dt IN DATE
                         ) RETURN OBJ_DLY_BILNG_ADJSTMNT_TABLE PIPELINED AS
--- TODO add optional input DLY_BILNG_ID_List as NUMBER_ARRAY and filtering by IN(DLY_BILNG_ID_List)                        
+-- TODO add optional input DLY_BILNG_ID_List as NUMBER_ARRAY and filtering by IN(DLY_BILNG_ID_List)
+-- TODO Create WITH for A if possible
   CURSOR cc IS
+    WITH SDB AS 
+      (SELECT DLY_BILNG_ID
+         FROM DLY_BILNG where MRKT_ID=p_mrkt_id AND OFFR_PERD_ID=p_offr_perd_id AND SLS_PERD_ID = p_sls_perd_id
+          AND trunc(PRCSNG_DT) = trunc(p_prcsng_dt)    
+      ),
+    CMP_OFFER AS
+      (SELECT OFFR_ID FROM OFFR
+         WHERE MRKT_ID=p_mrkt_id AND VER_ID = 0 AND OFFR_TYP='CMP'
+           AND OFFR_PERD_ID =p_offr_perd_id
+      ),
+    PLANNED AS
+      (SELECT SKU_ID FROM OFFR_SKU_LINE OSL 
+              WHERE OSL.OFFR_ID in( SELECT OFFR_ID FROM CMP_OFFER)
+               AND OSL.DLTD_IND NOT IN ('Y','y')
+      )
     SELECT OBJ_DLY_BILNG_ADJSTMNT_LINE(
       DB.DLY_BILNG_ID,
       DB.FSC_CD,
@@ -48,25 +67,20 @@ create or replace PACKAGE BODY PA_DLY_BILNG_ADJSTMNT AS
                 AND (PA_MAPS_PUBLIC.get_sls_cls_cd(p_offr_perd_id, p_mrkt_id, s.avlbl_perd_id,
                          s.intrdctn_perd_id, s.demo_ofs_nr, s.demo_durtn_nr, s.new_durtn_nr,
                          s.stus_perd_id, s.dspostn_perd_id, s.on_stus_perd_id)!='-1')),'1','A','N'),
-	    DECODE((SELECT count(*) FROM OFFR_SKU_LINE OSL 
-              WHERE OSL.OFFR_ID in( SELECT OFFR_ID FROM OFFR O
-                                    WHERE O.MRKT_ID=p_mrkt_id
-                                     AND O.VER_ID = 0
-                                     AND O.OFFR_TYP='CMP'
-                                     AND O.OFFR_PERD_ID =p_offr_perd_id)
-               AND OSL.DLTD_IND NOT IN ('Y','y')
-               AND OSL.SKU_ID=S.SKU_ID),0,'N','P'),
+	    NVL2(PLANNED.SKU_ID,'P','N'),
       DB.UNIT_QTY,
       DBAT.UNIT_QTY,
-      DBAT.LAST_UPDT_USER_ID,
-      DBAT.LAST_UPDT_TS) cline
-    FROM DLY_BILNG DB
+      DBAT.LAST_UPDT_TS,
+      U.USER_FRST_NM || ' ' || U.USER_LAST_NM) cline
+    FROM SDB
+      JOIN DLY_BILNG DB on DB.DLY_BILNG_ID=SDB.DLY_BILNG_ID
       LEFT JOIN MRKT_SKU S
         ON S.MRKT_ID=p_mrkt_id AND S.SKU_ID=DB.SKU_ID
+      LEFT JOIN PLANNED ON PLANNED.sku_id=DB.SKU_ID
       LEFT JOIN DLY_BILNG_ADJSTMNT DBAT
         ON DBAT.DLY_BILNG_ID=DB.DLY_BILNG_ID
-    WHERE DB.MRKT_ID=p_mrkt_id AND DB.SLS_PERD_ID=p_sls_perd_id
-      AND DB.OFFR_PERD_ID=p_offr_perd_id AND trunc(DB.PRCSNG_DT)=trunc(p_prcsng_dt)
+      LEFT JOIN MPS_USER U
+        ON U.USER_NM=DBAT.LAST_UPDT_USER_ID  
 --    ORDER BY DB.DLY_BILNG_ID,db.SKU_ID
     ;
   BEGIN
@@ -76,6 +90,61 @@ create or replace PACKAGE BODY PA_DLY_BILNG_ADJSTMNT AS
     END LOOP; 
   END GET_DLY_BILNG_ADJSTMNT;
   
+  FUNCTION GET_DLY_BILNG_ADJSTMNT2(p_dly_bilng_id_list IN NUMBER_ARRAY)
+    RETURN OBJ_DLY_BILNG_ADJSTMNT_TABLE PIPELINED AS
+  CURSOR cc IS
+    WITH SDB AS 
+      (select column_value DLY_BILNG_ID from table( p_dly_bilng_id_list)     
+      )
+    SELECT OBJ_DLY_BILNG_ADJSTMNT_LINE(
+      DB.DLY_BILNG_ID,
+      DB.FSC_CD,
+      PA_MAPS_PUBLIC.get_fsc_desc(DB.MRKT_ID,DB.OFFR_PERD_ID,DB.FSC_CD),
+      DB.SKU_ID,
+      S.LCL_SKU_NM,
+      DB.SLS_PRC_AMT,
+      DB.NR_FOR_QTY,
+      DB.SLS_PRC_AMT/DB.NR_FOR_QTY,
+--      'A',
+      DECODE((SELECT 1 FROM dual WHERE
+                EXISTS( SELECT SKU_PRC_AMT FROM MRKT_PERD_SKU_PRC MPSP
+                   WHERE MPSP.MRKT_ID=DB.MRKT_ID AND MPSP.OFFR_PERD_ID=DB.OFFR_PERD_ID
+                    AND MPSP.SKU_ID=S.SKU_ID AND MPSP.PRC_LVL_TYP_CD='RP' )
+                AND EXISTS( SELECT HOLD_COSTS_IND FROM SKU_COST SC 
+                   WHERE SC.MRKT_ID=DB.MRKT_ID AND SC.OFFR_PERD_ID=DB.OFFR_PERD_ID
+                    AND SC.SKU_ID=S.SKU_ID AND COST_TYP='P')
+                AND (PA_MAPS_PUBLIC.get_sls_cls_cd(DB.OFFR_PERD_ID, DB.MRKT_ID, s.avlbl_perd_id,
+                         s.intrdctn_perd_id, s.demo_ofs_nr, s.demo_durtn_nr, s.new_durtn_nr,
+                         s.stus_perd_id, s.dspostn_perd_id, s.on_stus_perd_id)!='-1')),'1','A','N'),
+      DECODE((SELECT count(*) FROM OFFR_SKU_LINE OSL 
+              WHERE OSL.OFFR_ID in( SELECT OFFR_ID FROM OFFR O
+                                    WHERE O.MRKT_ID=DB.MRKT_ID
+                                     AND O.VER_ID = 0
+                                     AND O.OFFR_TYP='CMP'
+                                     AND O.OFFR_PERD_ID =DB.OFFR_PERD_ID)
+               AND OSL.DLTD_IND NOT IN ('Y','y')
+               AND OSL.SKU_ID=S.SKU_ID),0,'N','P'),
+      DB.UNIT_QTY,
+      DBAT.UNIT_QTY,
+      DBAT.LAST_UPDT_TS,
+      U.USER_FRST_NM || ' ' || U.USER_LAST_NM) cline
+    FROM SDB
+      JOIN DLY_BILNG DB on DB.DLY_BILNG_ID=SDB.DLY_BILNG_ID
+      LEFT JOIN MRKT_SKU S
+        ON S.MRKT_ID=DB.MRKT_ID AND S.SKU_ID=DB.SKU_ID
+      LEFT JOIN DLY_BILNG_ADJSTMNT DBAT
+        ON DBAT.DLY_BILNG_ID=DB.DLY_BILNG_ID
+      LEFT JOIN MPS_USER U
+        ON U.USER_NM=DBAT.LAST_UPDT_USER_ID  
+--    ORDER BY DB.DLY_BILNG_ID,db.SKU_ID
+    ;
+  BEGIN
+
+    FOR rec in cc LOOP
+      pipe row(rec.cline);
+    END LOOP;
+END GET_DLY_BILNG_ADJSTMNT2;
+
   PROCEDURE SET_DLY_BILNG_ADJSTMNT(p_dly_bilng_id IN NUMBER,
                          p_new_bi24_units IN NUMBER,
                          p_user_id IN VARCHAR2,
