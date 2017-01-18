1fe0a526d8df94675de93f8fe422580c1314b8fe
aszabados <atttila.szabados@blackbelt.hu>
2017-01-18 14:11:17 +0100

    20170118

--- MAINTAIN_config_item___mrkt_config_item.sql
+++ MAINTAIN_config_item___mrkt_config_item.sql
@@ -1,11 +1,3 @@
--- BEFORE
--- config_item
-SELECT * FROM config_item where config_item_id > 10000 ORDER BY config_item_id;
--- mrkt_config_item
-SELECT *
-  FROM mrkt_config_item
- WHERE config_item_id > 10000
- ORDER BY config_item_id, mrkt_id, mrkt_config_item_val_txt;
 -- UPDATE
 -- config_item
 insert into config_item (CONFIG_ITEM_ID, CONFIG_ITEM_DESC_TXT, CONFIG_ITEM_LABL_TXT, SEQ_NR)
@@ -18,27 +10,13 @@ UPDATE mrkt_config_item
 -- INSERT
 insert into mrkt_config_item (MRKT_ID, CONFIG_ITEM_ID, MRKT_CONFIG_ITEM_DESC_TXT, MRKT_CONFIG_ITEM_LABL_TXT, MRKT_CONFIG_ITEM_VAL_TXT)
 values (73, 12005, 'TREND ALLOCATION ENABLED (73)', 'TREND ALLOCATION ENABLED (73)', 'Y');
--- AFTER
--- mrkt_config_item
-SELECT *
-  FROM mrkt_config_item
- WHERE config_item_id > 10000
- ORDER BY config_item_id, mrkt_id, mrkt_config_item_val_txt;
--- config_item
-SELECT *
-  FROM config_item
- WHERE config_item_id > 10000
- ORDER BY config_item_id;
-----------------------------
 -- AUDIT:
 -- config_item_audit
-select * from config_item_audit where config_item_id > 10000 ORDER BY config_item_id;
 -- INSERT:
 -- config_item_audit
 insert into config_item_audit (CONFIG_ITEM_ID, CONFIG_ITEM_DESC_TXT, CONFIG_ITEM_LABL_TXT, SEQ_NR)
 values (12005, 'TREND ALLOCATION ENABLED (73)', 'TREND ALLOCATION ENABLED (73)', 100);
 -- mrkt_config_item_audit
-select * from mrkt_config_item_audit where config_item_id > 10000 ORDER BY config_item_id;
 -- INSERT:
 insert into mrkt_config_item_audit (MRKT_ID, CONFIG_ITEM_ID, MRKT_CONFIG_ITEM_DESC_TXT, MRKT_CONFIG_ITEM_LABL_TXT, MRKT_CONFIG_ITEM_VAL_TXT)
 values (73, 12003, 'TREND ALLOCATION ENABLED', 'TREND ALLOCATION ENABLED', 'N');
--- table_types.sql
+++ table_types.sql
@@ -1,7 +1,7 @@
 create or replace TYPE OBJ_CASH_VAL_MANTNC_LINE as object
 ( SLS_PERD_ID number(8),
      CASH_VAL number(15,2),
-     SCT_R_FACTOR number(6,4),
+     SCT_R_FACTOR number(19,4),
      UPDT_IND varchar2(35),
      LAST_UPDT_TS date,
      LAST_UPDT_USER_ID varchar2(35)
