3b9580911664c318ea195f9d84552e91a9befdd1
aszabados <atttila.szabados@blackbelt.hu>
2017-01-18 14:34:23 +0100

    20171118

--- MAINTAIN_config_item___mrkt_config_item.sql
+++ MAINTAIN_config_item___mrkt_config_item.sql
@@ -17,9 +17,55 @@ values (73, 12005, 'TREND ALLOCATION ENABLED (73)', 'TREND ALLOCATION ENABLED (7
 insert into config_item_audit (CONFIG_ITEM_ID, CONFIG_ITEM_DESC_TXT, CONFIG_ITEM_LABL_TXT, SEQ_NR)
 values (12005, 'TREND ALLOCATION ENABLED (73)', 'TREND ALLOCATION ENABLED (73)', 100);
 -- mrkt_config_item_audit
--- INSERT:
+-- INSERT
 insert into mrkt_config_item_audit (MRKT_ID, CONFIG_ITEM_ID, MRKT_CONFIG_ITEM_DESC_TXT, MRKT_CONFIG_ITEM_LABL_TXT, MRKT_CONFIG_ITEM_VAL_TXT)
 values (73, 12003, 'TREND ALLOCATION ENABLED', 'TREND ALLOCATION ENABLED', 'N');
 -- mrkt_config_item_audit
 insert into mrkt_config_item_audit (MRKT_ID, CONFIG_ITEM_ID, MRKT_CONFIG_ITEM_DESC_TXT, MRKT_CONFIG_ITEM_LABL_TXT, MRKT_CONFIG_ITEM_VAL_TXT)
 values (73, 12005, 'TREND ALLOCATION ENABLED (73)', 'TREND ALLOCATION ENABLED (73)', 'Y');
+--
+--
+--
+insert into CONFIG_ITEM (CONFIG_ITEM_ID, CONFIG_ITEM_DESC_TXT, CONFIG_ITEM_LABL_TXT, SEQ_NR)
+values (10000, 'Sales Type Id for Trend Allocation', 'Sales Type Id for Trend Allocation', 999);
+--
+INSERT INTO mrkt_config_item
+  (mrkt_id,
+   config_item_id,
+   mrkt_config_item_desc_txt,
+   mrkt_config_item_labl_txt,
+   mrkt_config_item_val_txt)
+  SELECT mm.mrkt_id,
+         10000,
+         'Sales Type Id for Trend Allocation',
+         'Sales Type Id for Trend Allocation',
+         6
+    FROM mrkt mm
+   WHERE NOT EXISTS (SELECT mrkt_id
+            FROM mrkt_config_item mci
+           WHERE mci.mrkt_id = mm.mrkt_id
+             AND mci.config_item_id = 10000);
+-- AUDIT:
+-- config_item_audit
+-- INSERT:
+-- config_item_audit
+insert into config_item_audit (CONFIG_ITEM_ID, CONFIG_ITEM_DESC_TXT, CONFIG_ITEM_LABL_TXT, SEQ_NR)
+values (10000, 'Sales Type Id for Trend Allocation', 'Sales Type Id for Trend Allocation', 999);
+-- mrkt_config_item_audit
+-- INSERT
+INSERT INTO mrkt_config_item_audit
+  (mrkt_id,
+   config_item_id,
+   mrkt_config_item_desc_txt,
+   mrkt_config_item_labl_txt,
+   mrkt_config_item_val_txt)
+  SELECT mm.mrkt_id,
+         10000,
+         'Sales Type Id for Trend Allocation',
+         'Sales Type Id for Trend Allocation',
+         6
+    FROM mrkt mm
+   WHERE NOT EXISTS (SELECT mrkt_id
+            FROM mrkt_config_item mci
+           WHERE mci.mrkt_id = mm.mrkt_id
+             AND mci.config_item_id = 10000);
