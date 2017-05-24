e9cc20c89d1a09bb2b09ec88c768a2c2bba5e356
aszabados <atttila.szabados@blackbelt.hu>
2017-05-24 11:16:48 +0200

    Support Issue #1547

---
+++ Support/forecast_boost_1547/create_table/1 -New tables/c_frcst_boost_xclusn_mrkt_perd.sql
@@ -0,0 +1,78 @@
+﻿-- Create table
+create table FRCST_BOOST_XCLUSN_MRKT_PERD
+(
+  mrkt_id           NUMBER not null,
+  trgt_offr_perd_id NUMBER not null,
+  catgry_id         NUMBER,
+  sls_cls_cd        VARCHAR2(5),
+  sgmt_id           NUMBER,
+  creat_user_id     VARCHAR2(35) not null,
+  creat_ts          DATE not null,
+  last_updt_user_id VARCHAR2(35) not null,
+  last_updt_ts      DATE not null
+)
+tablespace &data_tablespace_name
+  pctfree 0
+  pctused 40
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+    pctincrease 0
+  )
+compress;
+-- Add comments to the table 
+comment on table FRCST_BOOST_XCLUSN_MRKT_PERD
+  is 'Forecast boost exclusion. This is for exclusions in the target market (not source market).
+A NULL Category assumes all Categories for the specified Sales Class and Segment are excluded.
+A NULL Sales Class assumes all Sales Classes for the specified Category and Segment are excluded.
+A NULL Segment assumes all Segments for the specified Category and Sales Class are excluded.
+All Category, Sales Class and Segment must not be null.';
+-- Add comments to the columns 
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.catgry_Id
+  is 'The Category to be excluded for forecast boost in the target market.';
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.sls_cls_cd
+  is 'The Sales Class to be excluded for forecast boost in the target market.';
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.sgmt_Id
+  is 'The Segment to be excluded for forecast boost in the target market.';
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.creat_user_id
+  is 'The MAPS Application USER ID of the user (person or process) that created the row.';
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.creat_ts
+  is 'The date and time the row was created.';
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.last_updt_user_id
+  is 'Contains the MAPS USER ID of the user (person or process) that last updated the row.';
+comment on column FRCST_BOOST_XCLUSN_MRKT_PERD.last_updt_ts
+  is 'The date and time the row was last updated.';
+-- Create/Recreate indexes 
+create unique index UI_FRCSTBSTXCLSNMRKTPRD_MPVCSS on FRCST_BOOST_XCLUSN_MRKT_PERD (MRKT_ID, TRGT_OFFR_PERD_ID, CATGRY_ID, SLS_CLS_CD, SGMT_ID)
+  tablespace &index_tablespace_name 
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 40K
+    next 40K
+    minextents 1
+    maxextents unlimited
+    pctincrease 50
+  )
+  compress 3;
+-- Create/Recreate primary, unique and foreign key constraints 
+alter table FRCST_BOOST_XCLUSN_MRKT_PERD
+  add constraint FK_SLSCLS_FRCSTBSTXCLSN_MP foreign key (SLS_CLS_CD)
+  references SLS_CLS (SLS_CLS_CD);
+alter table FRCST_BOOST_XCLUSN_MRKT_PERD
+  add constraint FK_CTGR_FRCSTBSTXCLSN_MP foreign key (CATGRY_ID)
+  references CATGRY (CATGRY_ID);
+alter table FRCST_BOOST_XCLUSN_MRKT_PERD
+  add constraint FK_SGMT_FRCSTBSTXCLSN_MP foreign key (SGMT_ID)
+  references SGMT (SGMT_ID);
+-- Grant/Revoke object privileges 
+grant select, insert, update, delete on FRCST_BOOST_XCLUSN_MRKT_PERD to CEMAPSP_ETL;
+grant select on FRCST_BOOST_XCLUSN_MRKT_PERD to MAPS_SUPPORT;
+grant select on FRCST_BOOST_XCLUSN_MRKT_PERD to MPSRO;
---
+++ Support/forecast_boost_1547/revert.sql
@@ -0,0 +1,30 @@
+﻿PROMPT
+PROMPT Reverting Support_Issue_1547
+PROMPT
+
+set feedback off termout off pagesize 0
+col log_file new_value log_file
+select user||'_'||instance_name||'_'||to_char(sysdate, 'yyyy-mm-dd')||'_revert_support_issue_1547.log' log_file from v$instance;
+set feedback on termout on
+
+spool '&log_file'
+
+set serveroutput on size 1000000 format wrapped
+set head off
+select user || '@' || instance_name || ' on ' || host_name from v$instance;
+select ' Start: ' || to_char(sysdate, 'yyyy-mm-dd hh24:mi:ss') from dual;
+set head on
+
+-- revert
+-- tables, indexes, triggers          (6 -Revert)
+@@"create_table/6 -Revert/drop_new_tables.sql"
+-- Copy_Offer     (revert to previous version)
+@@"Copy_Offer/revert_cpy_offr.pck"
+
+set head off
+select user || '@' || instance_name || ' on ' || host_name from v$instance;
+select ' End:   ' || to_char(sysdate, 'yyyy-mm-dd hh24:mi:ss') from dual;
+set head on
+
+PROMPT Reverted Support_Issue_1547
+spool off
