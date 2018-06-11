b846896fa072f0b9a64708e5357bcd1c78765682
csvarady <csvarady@gmail.com>
2018-06-11 09:45:59 +0200

    install.sql was added

---
+++ MAPS_EditOffr/install.sql
@@ -0,0 +1,13 @@
+@Types\OBJ_EDIT_OFFR_HIST_LINE.sql
+@Types\OBJ_EDIT_OFFR_HIST_TABLE.sql
+@Types\OBJ_EDIT_OFFR_LINE.sql
+@Types\OBJ_EDIT_OFFR_TABLE.sql
+
+@Tables\EDIT_OFFR_HIST.sql
+
+@Utils\insert_CONFIG_ITEM_MRKT_CONFIG_ITEM.sql 
+
+@Packages\PA_MAPS_EDIT_OFFR.sql
+show errors
+@Packages\PA_MAPS_EDIT_OFFR_body.sql
+show errors
