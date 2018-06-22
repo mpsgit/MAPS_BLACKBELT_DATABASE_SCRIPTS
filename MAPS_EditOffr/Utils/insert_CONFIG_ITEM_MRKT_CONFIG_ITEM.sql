9a17ecafbc3068231af609065f05212a6927f5d6
csvarady <csvarady@gmail.com>
2018-06-22 12:54:26 +0200

    script to create mrkt_config_item for more markets

--- MAPS_EditOffr/Utils/insert_CONFIG_ITEM_MRKT_CONFIG_ITEM.sql
+++ MAPS_EditOffr/Utils/insert_CONFIG_ITEM_MRKT_CONFIG_ITEM.sql
@@ -7,10 +7,12 @@ VALUES (9200, 'Offr default values for offer manipulation', 'Offr default values
 INSERT INTO config_item (config_item_id, config_item_desc_txt, config_item_labl_txt, seq_nr)
 VALUES (9210, 'Price point default values for offer manipulation', 'Price point default values for offer manipulation', 150);
 
+/*
 INSERT INTO mrkt_config_item (mrkt_id, config_item_id, mrkt_config_item_desc_txt, mrkt_config_item_labl_txt, mrkt_config_item_val_txt)
 VALUES (73, 9200, 'Offr default values for offer manipulation', 'Offr default values for offer manipulation', '200,1,0,0,0,0,1,CMP,N,0,2,1');
 
 INSERT INTO mrkt_config_item (mrkt_id, config_item_id, mrkt_config_item_desc_txt, mrkt_config_item_labl_txt, mrkt_config_item_val_txt)
 VALUES (73, 9210, 'Price point default values for offer manipulation', 'Offr default values for offer manipulation', '147,10,1,0,0,0,1,0,1,1,N,0,0,0,0,1');
+*/
 
 COMMIT;
---
+++ MAPS_EditOffr/Utils/insert_CONFIG_ITEM_MRKT_CONFIG_ITEM_for_more_mrkts.sql
@@ -0,0 +1,15 @@
+BEGIN
+
+  FOR I IN (SELECT DISTINCT mrkt_id FROM mrkt_perd) LOOP
+
+    INSERT INTO mrkt_config_item (mrkt_id, config_item_id, mrkt_config_item_desc_txt, mrkt_config_item_labl_txt, mrkt_config_item_val_txt)
+    VALUES (i.mrkt_id, 9200, 'Offr default values for offer manipulation', 'Offr default values for offer manipulation', '200,1,0,0,0,0,1,CMP,N,0,2,1');
+  
+    INSERT INTO mrkt_config_item (mrkt_id, config_item_id, mrkt_config_item_desc_txt, mrkt_config_item_labl_txt, mrkt_config_item_val_txt)
+    VALUES (i.mrkt_id, 9210, 'Price point default values for offer manipulation', 'Offr default values for offer manipulation', '147,10,1,0,0,0,1,0,1,1,N,0,0,0,0,1');
+
+    dbms_output.put_line(i.mrkt_id);
+  END LOOP;
+END;
+
+COMMIT;
