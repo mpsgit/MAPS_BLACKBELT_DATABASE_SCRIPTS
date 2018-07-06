1f8019bc8dcab6c26e5c666796d48bf1bc07dac3
csvarady <csvarady@gmail.com>
2018-07-06 15:20:46 +0200

    SIMS IMED - Manual Trend Upload #2306 - deleting

---
+++ SIMS/manual_trend_upload/Types/OBJ_MANL_TREND_UPL_DEL_LINE.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TYPE obj_manl_trend_upl_del_line FORCE AS OBJECT
+(
+  mrkt_id       NUMBER,
+  sls_perd_id   NUMBER,
+  sls_typ_id    NUMBER,
+  fsc_cd        VARCHAR2(5)
+);
+/
---
+++ SIMS/manual_trend_upload/Types/OBJ_MANL_TREND_UPL_DEL_TABLE.sql
@@ -0,0 +1,3 @@
+CREATE OR REPLACE TYPE obj_manl_trend_upl_del_table FORCE
+AS TABLE OF obj_manl_trend_upl_del_line;
+/
