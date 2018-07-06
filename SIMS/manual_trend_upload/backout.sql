9962dd79ae4e2ed5373e63902f3c2ad4cd36be23
csvarady <csvarady@gmail.com>
2018-07-06 15:47:42 +0200

    SIMS IMED - Manual Trend Upload #2306 - backout script was added

---
+++ SIMS/manual_trend_upload/backout.sql
@@ -0,0 +1,12 @@
+DROP PACKAGE BODY pa_manual_trend_upload;
+
+DROP PACKAGE pa_manual_trend_upload;
+
+DROP TYPE obj_manl_trend_upload_table;
+DROP TYPE obj_manl_trend_upload_line;
+DROP TYPE obj_manl_trend_upl_save_table;
+DROP TYPE obj_manl_trend_upl_save_line;
+DROP TYPE obj_manl_trend_upl_del_table;
+DROP TYPE obj_manl_trend_upl_del_line;
+
+DROP TABLE manual_trend_upload;
