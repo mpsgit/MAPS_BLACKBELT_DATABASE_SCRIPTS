576cc19cbfba26bd091d2450be694fa54aec40ad
csvarady <csvarady@gmail.com>
2018-08-13 15:35:15 +0200

    install.sql -> upgrade.sql

---
+++ MAPS_EditOffr/upgrade.sql
@@ -0,0 +1,20 @@
+@Types\OBJ_EDIT_OFFR_HIST_LINE.sql
+@Types\OBJ_EDIT_OFFR_HIST_TABLE.sql
+@Types\OBJ_EDIT_OFFR_LINE.sql
+@Types\OBJ_EDIT_OFFR_TABLE.sql
+@Types\OBJ_COPY_OFFR_LINE.sql
+@Types\OBJ_COPY_OFFR_TABLE.sql
+@Types\OBJ_EDIT_OFFR_FILTER_LINE.sql
+@Types\OBJ_EDIT_OFFR_FILTER_TABLE.sql
+@Types\OBJ_GET_OFFR_LINE.sql
+@Types\OBJ_GET_OFFR_TABLE.sql
+
+@Utils\insert_CONFIG_ITEM_MRKT_CONFIG_ITEM.sql
+@Utils\insert_CONFIG_ITEM_MRKT_CONFIG_ITEM_for_more_mrkts.sql
+@Utils\alter_table_EDIT_OFFR_HIST.sql
+
+@Packages\PA_MAPS_EDIT_OFFR.sql
+show errors
+
+@Packages\PA_MAPS_EDIT_OFFR_body.sql
+show errors
