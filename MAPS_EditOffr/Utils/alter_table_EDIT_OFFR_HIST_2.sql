060db47a257f1e57f65a81b689e2ab0cac9d5e54
csvarady <csvarady@gmail.com>
2018-08-31 16:24:14 +0200

    New columns were added to edit_offr

---
+++ MAPS_EditOffr/Utils/alter_table_EDIT_OFFR_HIST_2.sql
@@ -0,0 +1,7 @@
+ALTER TABLE edit_offr_hist ADD (
+  impct_catgry_id             NUMBER,
+  hero_ind                    CHAR(1),
+  smplg_ind                   CHAR(1),
+  mltpl_ind                   CHAR(1),
+  cmltv_ind                   CHAR(1)
+);
