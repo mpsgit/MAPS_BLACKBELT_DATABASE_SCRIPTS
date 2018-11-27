90a376dfb4507a44118b7d992e8897354b3be41e
csvarady <csvarady@gmail.com>
2018-11-27 15:15:24 +0100

    Backout was removed

--- MAPS_EditOffr/backout/Utils/revert_CONFIG_ITEM.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM mrkt_config_item WHERE config_item_id IN (9200, 9210, 9201);
-DELETE FROM config_item WHERE config_item_id IN (9200, 9210, 9201);
-
-COMMIT;
--- MAPS_EditOffr/backout/Utils/revert_EDIT_OFFR_HIST.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN offr_typ;
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN forcasted_units;
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN forcasted_date;
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN offr_cls_id;
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN spcl_ordr_ind;
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN offr_ofs_nr;
-ALTER TABLE edit_offr_hist SET UNUSED COLUMN pp_ofs_nr;
-
-ALTER TABLE edit_offr_hist DROP UNUSED COLUMNS;
